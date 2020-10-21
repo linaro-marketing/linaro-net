d678d8b69771945bc094673f240e78566ce403fa
Philip Colmer <philip.colmer@linaro.org>
2020-10-21 12:49:00 +0100

    Fix execSync

--- .github/actions/merge-test-branch/cleanup-branch.js
+++ .github/actions/merge-test-branch/cleanup-branch.js
@@ -1,17 +1,16 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { exec } = require('child_process');
+import { execSync } from 'child_process';
 
 try {
     const incoming_owner = github.context.payload.pull_request.head.repo.owner.login;
     const incoming_branch = github.context.payload.pull_request.head.ref;
-    const execSync = exec.execSync;
     execSync("cd $GITHUB_WORKSPACE");
-    console.log(execSync("git reset --merge"));
+    console.log(execSync("git reset --merge", { encoding: 'utf-8' }));
     console.log("git checkout $GITHUB_BASE_REF");
-    console.log(execSync("git checkout $GITHUB_BASE_REF"));
+    console.log(execSync("git checkout $GITHUB_BASE_REF", { encoding: 'utf-8' }));
     console.log(`git branch -D ${incoming_owner}-${incoming_branch}`);
-    console.log(execSync(`git branch -D ${incoming_owner}-${incoming_branch}`));
+    console.log(execSync(`git branch -D ${incoming_owner}-${incoming_branch}`, { encoding: 'utf-8' }));
 } catch (error) {
     core.setFailed(error.message);
 }
